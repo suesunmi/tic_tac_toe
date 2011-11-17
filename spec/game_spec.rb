828627d07afd2c0e212720c9346c0f964bf643ed
Sue Kim <suesunmi@gmail.com>
2011-11-17 15:27:20 -0600

    remove Board tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,4 @@
+require 'player'
 require 'game'
 
 describe Game do
@@ -5,14 +6,6 @@ describe Game do
     @player_1 = Player.new("Player A", "X")
     @player_2 = Player.new("Player B", "O")
     @game = Game.new(@player_1, @player_2)
-  end
-  it "records the play properly" do
-    @game.board.record_play(2, @player_1)
-    @game.board.plays[2].marker.should == @player_1.marker
-  end
-  it "displays the score accurately" do
-    @game.board.record_play(4, @player_2)
-    @game.board.display.should == "\n1:     |   2:     |   3:  \n4: #{@player_2.marker}   |   5:     |   6:  \n7:     |   8:     |   9:  \n"
   end
   it "toggles to Player B after the first play" do
     @game.next_play(1)
