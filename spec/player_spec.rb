c6c515215d3592f0d90efacb29e0092ad07de579
Sue Kim <suesunmi@gmail.com>
2011-12-06 14:18:13 -0600

    recognize opponent test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,13 @@
 require 'player'
+require 'board'
 
 describe Player do
+  it "recognizes its opponent on the board" do
+    this_is_me = Player.new("Me", "X")
+    this_is_him = Player.new("The Other Guy", "O")
+    board = Board.new
+    board.mark_position_with_marker(1, this_is_me.marker)
+    board.mark_position_with_marker(2, this_is_him.marker)
+    this_is_me.my_opponents_mark(board).should == this_is_him.marker
+  end
 end
