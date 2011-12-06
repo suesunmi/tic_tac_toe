894a51c9566f990897001b51f46ad244a5ac29fb
Sue Kim <suesunmi@gmail.com>
2011-12-06 14:20:23 -0600

    test random inputs and repeating plays

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,7 +17,20 @@ describe Game do
     @game.next_play(8)
     @game.should_not be_over
   end
-  it "doesn't overwrite already-made plays"
-  it "advances game correctly when input is nothing"
-  it "advances game correctly when input is random"
+  it "doesn't overwrite already-made plays" do
+    first_player = @game.current_player
+    @game.next_play(7)
+    @game.next_play(7)
+    @game.board.plays[7] == first_player.marker
+  end
+  it "advances game correctly when input is nothing" do
+#    first_player = @game.current_player
+#    @game.next_play()  WHERE SHOULD I TRAP FOR NUMBER OF ARGS ERROR?
+#    @game.current_player.should == first_player
+  end
+  it "advances game correctly when input is random" do
+    first_player = @game.current_player
+    @game.next_play('dunno')
+    @game.current_player.should == first_player
+  end
 end
