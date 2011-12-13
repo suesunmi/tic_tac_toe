114abdfc25a123abfaab20850642433b1008f786
Sue Kim <suesunmi@gmail.com>
2011-12-13 12:10:09 -0600

    uncomment now passing tests

--- spec/best_player_spec.rb
+++ spec/best_player_spec.rb
@@ -30,25 +30,25 @@ describe BestPlayer do
     mark_multiple_positions(@board, Hash[ 1, @computer.marker, 5, @human.marker, 3, @computer.marker, 2, @human.marker ] )
     @computer.decide_next_move(@board).should == 8
   end
-#  it "blocks opponent from winning with 5 plays left - ii" do
+  it "blocks opponent from winning with 5 plays left - ii" do
 #1:     |   2:     |   3: X
 #4:     |   5: O   |   6: O
 #7:     |   8:     |   9: X
-#    mark_multiple_positions(@board, Hash[ 3, @computer.marker, 5, @human.marker, 9, @computer.marker, 6, @human.marker ] )
-#    @computer.decide_next_move(@board).should == 4
-#  end
-#  it "chooses to win rather than block opponent" do
+    mark_multiple_positions(@board, Hash[ 3, @computer.marker, 5, @human.marker, 9, @computer.marker, 6, @human.marker ] )
+    @computer.decide_next_move(@board).should == 4
+  end
+  it "chooses to win rather than block opponent - i" do
 #1: X   |   2: O   |   3:
 #4: X   |   5: O   |   6:
 #7:     |   8:     |   9:
-#    mark_multiple_positions(@board, Hash[ 1, @computer.marker, 2, @human.marker, 4, @computer.marker, 5, @human.marker ] )
-#    @computer.decide_next_move(@board).should == 7
-#  end
-#  it "chooses to win rather than block opponent 2" do
+    mark_multiple_positions(@board, Hash[ 1, @computer.marker, 2, @human.marker, 4, @computer.marker, 5, @human.marker ] )
+    @computer.decide_next_move(@board).should == 7
+  end
+  it "chooses to win rather than block opponent - ii" do
 #1:     |   2: O   |   3: X
 #4:     |   5: O   |   6: X
 #7:     |   8:     |   9:
-#    mark_multiple_positions(@board, Hash[ 3, @computer.marker, 2, @human.marker, 6, @computer.marker, 5, @human.marker ] )
-#    @computer.decide_next_move(@board).should == 9
-#  end
+    mark_multiple_positions(@board, Hash[ 3, @computer.marker, 2, @human.marker, 6, @computer.marker, 5, @human.marker ] )
+    @computer.decide_next_move(@board).should == 9
+  end
 end
